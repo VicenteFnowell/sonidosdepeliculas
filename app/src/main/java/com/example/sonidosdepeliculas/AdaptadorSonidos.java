296284cc4a043739c2dff55ebc5178596ea6df31
vicentefn <vicentefnowell@gmail.com>
2018-03-13 00:28:27 +0100

    Proyecto a falta de botón share, la función introducida no es correcta

--- app/src/main/java/com/example/sonidosdepeliculas/AdaptadorSonidos.java
+++ app/src/main/java/com/example/sonidosdepeliculas/AdaptadorSonidos.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -35,6 +37,7 @@ public class AdaptadorSonidos extends ArrayAdapter<CSonidos> {
     ArrayList<CSonidos> sonidos;
     Context c;
     StorageReference stRef;
+    StorageReference audioRef;
 
     public AdaptadorSonidos(Context c, ArrayList<CSonidos> sonidos){
         super(c,R.layout.item_sonidospelis,sonidos);
@@ -69,8 +72,12 @@ public class AdaptadorSonidos extends ArrayAdapter<CSonidos> {
 
                 String sonido = sonidos.get(position).getNombre();
                 String pelicula = sonidos.get(position).getPelicula();
+                try {
+                    compartirAudio(sonido,pelicula);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-                compartirAudio(sonido,pelicula);
 
             }
         });
@@ -122,55 +129,31 @@ public class AdaptadorSonidos extends ArrayAdapter<CSonidos> {
         });
     }
 
-/*
-    private void compartirAudio(final String audio, String pelicula){
 
-        final String urlaudio = stRef.child("audios/"+pelicula+("/")+audio).getDownloadUrl().toString();
-        stRef.child("audios/"+pelicula+("/")+audio).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-
-                String sonidocompartido = audiotemporal(getContext(),urlaudio).toString();
-
-                uri = Uri.parse(sonidocompartido);
-                Intent share = new Intent(Intent.ACTION_SEND);
-                share.setType("audio/*");
-                share.putExtra(Intent.EXTRA_STREAM,uri);
-                c.startActivity(Intent.createChooser(share, "Compartir sonido en: "));
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
-            }
-        });
-    }
-*/
-
-
-    private void compartirAudio(final String audio, String pelicula){
-
-        String urlaudio = stRef.child("audios/"+pelicula+("/")+audio).getDownloadUrl().toString();
-        String sonidocompartido = audiotemporal(getContext(),urlaudio).toString();
-        Uri uri = Uri.parse(sonidocompartido);
-        Intent share = new Intent(Intent.ACTION_SEND);
-        share.setType("audio/mp3");
-        share.putExtra(Intent.EXTRA_STREAM,uri);
-        c.startActivity(Intent.createChooser(share, "Compartir sonido en: "));
-    }
-
-    private File audiotemporal (Context context,String url){
-        File file = null;
-        try {
-            String audio = Uri.parse(url).getLastPathSegment();
-            file=File.createTempFile(audio,null,context.getCacheDir());
-        }catch (IOException e){
-            //Error while creating file
+private void compartirAudio (String audio, String pelicula) throws IOException {
+    StorageReference audiotemporal = stRef.child("audios/"+pelicula+("/")+audio);
+    final File audiodescargado;
+    audiodescargado = File.createTempFile("audios","mp3");
+    audiotemporal.getFile(audiodescargado).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+        @Override
+        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+            String audiotempdescargado = audiodescargado.toString();
+            Uri uri = Uri.parse(audiotempdescargado);
+            Intent share =  new Intent(Intent.ACTION_SEND);
+            share.setType("audio/*");
+            share.putExtra(Intent.EXTRA_STREAM,uri);
+            c.startActivity(Intent.createChooser(share,"Compartir sonido en: "));
+            //Local temp file has been created
         }
-        return file;
-    }
+    }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            //Handle any errors
+        }
+    });
 
+
+}
 
 /*
     private void compartirAudio (String audio, String pelicula){
@@ -183,6 +166,11 @@ public class AdaptadorSonidos extends ArrayAdapter<CSonidos> {
         c.startActivity(Intent.createChooser(share, "Compartir sonido en: "));
 
     }
-*/
+    */
+
 
 }
+
+
+
+
